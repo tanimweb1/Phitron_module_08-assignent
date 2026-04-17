501b74f94bc67b7adb84e2e82b37ee33b94426f6
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-17 12:36:14 +0600

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -11,3 +11,37 @@ int main(){
 
 return 0;
 }
+
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+  //02
+    int n;
+    scanf("%d",&n);
+
+if(n>0){
+
+   for(int i = 1;i<=n;i++){
+    printf("%d ",i);}
+
+}
+ 
+
+ else if(n<=0){
+  for(int i = n; i<=0;i++){
+    printf("%d ",i);
+  }
+}
+    
+
+
+
+    
+
+return 0;
+}
